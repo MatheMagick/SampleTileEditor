732a6bac3984fdc46bdfdc76339fe3effe74d09b
MatheMagick <vassilev.vlad@gmail.com>
2015-07-25 19:16:53 +0300

    missed files

---
+++ UbisoftTileEditor/Converters/BoolToCursorConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace UbisoftTileEditor.Converters
+{
+    public sealed class BoolToCursorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool objectValue = (bool) value;
+            return objectValue ? Cursors.Arrow : Cursors.Cross;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
